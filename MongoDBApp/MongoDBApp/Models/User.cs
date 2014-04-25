a85478326098d4ea7a7be59f48fda017dc5ce6a8
tempuser101 <tempuser101@users.noreply.github.com>
2014-04-25 10:35:27 +0530

    added missing file

---
+++ MongoDBApp/MongoDBApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace MongoDBApp.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public int Age { get; set; }
+    }
+}
